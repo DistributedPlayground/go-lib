8de93d240c2353bf7484ad7f860d8606543bb774
Ocasta <ocasta181@gmail.com>
2023-08-10 00:35:41 -0500

    add error file to common

---
+++ common/error.go
@@ -0,0 +1,64 @@
+package common
+
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/labstack/echo/v4"
+	"github.com/pkg/errors"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+)
+
+func LogError(c echo.Context, err error, handlerMsg string) {
+	lg := c.Get("logger").(*zerolog.Logger)
+	lg.Error().Stack().Err(err).Msg(handlerMsg)
+}
+
+func LogStringError(c echo.Context, err error, handlerMsg string) {
+	type stackTracer interface {
+		StackTrace() errors.StackTrace
+	}
+
+	tracer, ok := errors.Cause(err).(stackTracer)
+	if !ok {
+		log.Warn().Str("error", err.Error()).Msg("error does not implement stack trace")
+		return
+	}
+
+	cause := errors.Cause(err)
+	st := tracer.StackTrace()
+
+	serviceName := os.Getenv("SERVICE_NAME")
+	if serviceName == "" {
+		log.Warn().Msg("service name is missing from env")
+	}
+
+	if IsLocalEnv() {
+		st2 := fmt.Sprintf("\nSTACK TRACE:\n%+v: [%+v ]\n\n", cause.Error(), st)
+		st2 = strings.ReplaceAll(st2, "/"+serviceName+"/", "")
+		fmt.Print(st2)
+		return
+	}
+
+	LogError(c, err, handlerMsg)
+}
+
+func DPError(err error, optionalMsg ...string) error {
+	if err == nil {
+		return nil
+	}
+
+	concat := ""
+
+	for _, msgs := range optionalMsg {
+		concat += msgs + " "
+	}
+
+	if errors.Cause(err) == nil || errors.Cause(err).Error() == err.Error() {
+		return errors.Wrap(errors.New(err.Error()), concat)
+	}
+
+	return errors.Wrap(err, concat)
+}
--- go.mod
+++ go.mod
@@ -3,19 +3,22 @@ module github.com/DistributedPlayground/go-lib
 go 1.19
 
 require (
+	github.com/go-playground/validator/v10 v10.15.0
+	github.com/jmoiron/sqlx v1.3.5
+	github.com/joho/godotenv v1.5.1
+	github.com/labstack/echo/v4 v4.11.1
+	github.com/pkg/errors v0.9.1
+	github.com/rs/zerolog v1.30.0
+)
+
+require (
 	github.com/gabriel-vasile/mimetype v1.4.2 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
-	github.com/go-playground/validator/v10 v10.15.0 // indirect
-	github.com/jmoiron/sqlx v1.3.5 // indirect
-	github.com/joho/godotenv v1.5.1 // indirect
-	github.com/labstack/echo/v4 v4.11.1 // indirect
 	github.com/labstack/gommon v0.4.0 // indirect
 	github.com/leodido/go-urn v1.2.4 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.19 // indirect
-	github.com/pkg/errors v0.9.1 // indirect
-	github.com/rs/zerolog v1.30.0 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
 	golang.org/x/crypto v0.11.0 // indirect
